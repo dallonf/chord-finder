d83044611c98d13e8775defb7694115bdfeaa36f
DallonF <dallon.feldner@gmail.com>
2018-07-07 11:45:18 -0500

    Display all major triads

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,53 @@
 import * as React from 'react';
-import './App.css';
+import * as _ from 'lodash';
+import { MIDDLE_C, Interval, C_MAJOR, NoteIdentity } from './theory/types';
+import { majorTriad } from './theory/chords';
+import { absoluteNoteToIdentity } from './theory/conversion';
 
-import logo from './logo.svg';
+const ALL_TRIADS = _.flatMap(_.range(MIDDLE_C, Interval.OCTAVE), note => [
+  {
+    root: absoluteNoteToIdentity(note, C_MAJOR),
+    type: 'major',
+    notes: majorTriad(note).map(x => absoluteNoteToIdentity(x, C_MAJOR)),
+  },
+]);
+console.log(ALL_TRIADS);
+
+const Note = ({ note }: { note: NoteIdentity }) => (
+  <span>
+    {note.name}
+    {(() => {
+      switch (note.type) {
+        case 'flat':
+          return 'b';
+        case 'sharp':
+          return '#';
+      }
+      return undefined;
+    })()}
+  </span>
+);
 
 class App extends React.Component {
   public render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
+      <div>
+        <h1>Every Triad Ever</h1>
+        <ul>
+          {ALL_TRIADS.map((x, i) => (
+            <li key={i}>
+              <strong>
+                <Note note={x.root} />:
+              </strong>{' '}
+              {x.notes.map((y, i) => (
+                <React.Fragment key={i}>
+                  <Note note={y} />
+                  {i !== x.notes.length - 1 && ' '}
+                </React.Fragment>
+              ))}
+            </li>
+          ))}
+        </ul>
       </div>
     );
   }
